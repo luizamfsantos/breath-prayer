47e688d8879a99f49281d8c40ab8edd97264a12c
luizamfsantos <luiza.marques_@hotmail.com>
2026-01-04 07:14:08 -0300

    feat(domain): add prayer schedule

---
+++ LocalPackages/Domain/Models/PrayerSchedule.swift
@@ -0,0 +1,14 @@
+//
+//  PrayerSchedule.swift
+//  Breath Prayer
+//
+//  Created by Luiza Santos on 1/4/26.
+//
+
+import Foundation
+
+struct PrayerSchedule: Codable {
+    var startTime: DateComponents
+    var endTime: DateComponents
+    var intervalMinutes: Int
+}
