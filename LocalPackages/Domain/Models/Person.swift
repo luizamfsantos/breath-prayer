326d34ef3ba78a557d323ca5d78bd306da77ecbd
luizamfsantos <luiza.marques_@hotmail.com>
2026-01-04 07:11:53 -0300

    feat(domain): add person

---
+++ LocalPackages/Domain/Models/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  Breath Prayer
+//
+//  Created by Luiza Santos on 1/4/26.
+//
+
+import Foundation
+
+struct Person: Identifiable, Codable {
+    let id: UUID
+    var name:  String
+}
