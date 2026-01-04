ea0fa06c6ec57765d603ed31ef1b45237d4a76b0
luizamfsantos <luiza.marques_@hotmail.com>
2026-01-04 07:04:57 -0300

    Initial Commit

---
+++ Breath Prayer/Breath_PrayerApp.swift
@@ -0,0 +1,32 @@
+//
+//  Breath_PrayerApp.swift
+//  Breath Prayer
+//
+//  Created by Luiza Santos on 1/4/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Breath_PrayerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Breath PrayerTests/Breath_PrayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Breath_PrayerTests.swift
+//  Breath PrayerTests
+//
+//  Created by Luiza Santos on 1/4/26.
+//
+
+import Testing
+@testable import Breath_Prayer
+
+struct Breath_PrayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
